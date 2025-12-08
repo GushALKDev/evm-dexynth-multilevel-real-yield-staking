d947d929d7a8ce37f16eb10564d7784da7d64cdb
GushALKDev <gustavomartindeveloper@gmail.com>
2025-12-08 20:26:33 +0100

    Refactor code structure for improved readability and maintainability

--- src/DexynthStaking.sol
+++ src/DexynthStaking.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/token/ERC20/IERC20.sol";
+import "@openzeppelin/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/access/Ownable.sol";
+import "@openzeppelin/security/ReentrancyGuard.sol";
 
 contract DexynthStakingV1 is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
